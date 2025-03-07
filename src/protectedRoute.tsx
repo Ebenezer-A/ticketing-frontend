79eddc51522c9c2405ac164fb0961751f4df5d6b
Ebenezer-A <abenezerame@gmail.com>
2025-03-08 01:27:28 +0300

    unused variable cleanup

--- src/protectedRoute.tsx
+++ src/protectedRoute.tsx
@@ -1,14 +1,7 @@
 import { Navigate, Outlet } from "react-router";
-import { useSelector } from "react-redux";
-import { RootState } from "./store/store";
 
 const ProtectedRoute = () => {
-  const { isAuthenticated } = useSelector(
-    (state: RootState) => state.userReducer
-  );
-
   const token = localStorage.getItem("token");
-  console.log("-----t", token);
   if (!token) {
     return <Navigate to="/login" replace />;
   }
