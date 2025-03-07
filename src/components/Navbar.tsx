31b358444f247daa0ff23c4f1d378b1b67d647c2
Ebenezer-A <abenezerame@gmail.com>
2025-03-08 01:54:54 +0300

    logout added to profile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../store/store";
 import { Plus } from "lucide-react";
+import { useState } from "react";
 
 type Props = {
   onClick: () => void;
@@ -11,10 +12,30 @@ function Navbar({ onClick }: Props) {
     (state: RootState) => state.userReducer.user
   );
 
+  const [logout, setLogout] = useState(false);
+
   return (
     <div className=" flex justify-between items-center bg-background px-5 py-3">
-      <div className="ml-3 rounded-full bg-white p-2 text-2xl ">
-        {name.charAt(0).toUpperCase() + name.charAt(1).toLowerCase()}
+      <div className="relative ml-3">
+        <div
+          onClick={() => setLogout(!logout)}
+          className="rounded-full bg-white p-2 text-2xl"
+        >
+          {name.charAt(0).toUpperCase() + name.charAt(1).toLowerCase()}
+        </div>
+        {logout && (
+          <div className="absolute mt-2 shadow-gray-200 bg-white rounded-md shadow-lg py-1">
+            <button
+              onClick={() => {
+                localStorage.clear();
+                window.location.reload();
+              }}
+              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left"
+            >
+              Logout
+            </button>
+          </div>
+        )}
       </div>
 
       <div className="text-white text-3xl font-extralight">
